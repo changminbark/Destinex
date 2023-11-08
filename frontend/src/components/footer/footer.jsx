00d18febdca214302dd22ca14461f3a387618e15
Chang Min Bark <cb073@bucknell.edu>
2023-11-08 10:30:32 -0500

    finished footer html

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./footer.css"
+
+import Appstore_Icon from "../../assets/svg/appstore_icon.svg"
+import Facebook_Icon from "../../assets/svg/facebook_icon.svg"
+import Instagram_Icon from "../../assets/svg/instagram_icon.svg"
+import Linkedin_Icon from "../../assets/svg/linkedin_icon.svg"
+import X_Icon from "../../assets/svg/x_icon.svg"
+import Youtube_Icon from "../../assets/svg/youtube_icon.svg"
+
+function Footer() {
+    return (
+        <div className={"bottom"}>
+            <div className={"bottomTitle"}>
+                <span className={"bottomTitleText"}>Destinex</span>
+            </div>
+            <div className={"bottomLinksContainer"}>
+                <img className={"Appstore_Logo"} src={Appstore_Icon} alt={"Appstore Logo"} />
+                <div className={"bottomLinks"}>
+                    <span className={"bottomLinksText"}>About Destinex</span>
+                    <span className={"bottomLinksText"}>Our Portfolio</span>
+                    <span className={"bottomLinksText"}>Careers</span>
+                    <span className={"bottomLinksText"}>Contact</span>
+                    <span className={"bottomLinksText"}>Help Center</span>
+                </div>
+                <div className={"bottomLinks"}>
+                    <span className={"bottomLinksText"}>Sign up to grant</span>
+                    <span className={"bottomLinksText"}>Privacy Policy</span>
+                    <span className={"bottomLinksText"}>Terms and Conditions</span>
+                </div>
+            </div>
+            <div className={"bottomSocMed"}>
+                <span className={"bottomSocMedFollowText"}>Follow Destinex</span>
+                <img src={Facebook_Icon} alt={"Facebook Logo"} />
+                <img src={Instagram_Icon} alt={"Instagram Logo"} />
+                <img src={Linkedin_Icon} alt={"Linkedin Logo"} />
+                <img src={X_Icon} alt={"X Logo"} />
+                <img src={Youtube_Icon} alt={"Youtube Logo"} />
+            </div>
+            <div className={"bottomCopyright"}>
+                <span className={"bottomCopyrightText"}>@2023 Destinex. All Rights Reserved.</span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
