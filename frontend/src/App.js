00a4c5af7d9413ffdc04460d1607d56cb97b2e8c
nl020 <nl020@bucknell.edu>
2023-11-11 16:02:52 -0500

    Add setup page for granters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,12 +15,14 @@ import SigninWisher from './pages/wishers/signin/signin';
 
 // Import files for granters
 import SignupGranter from './pages/granters/signup/signup';
+import SetupPage from './pages/granters/setup/setup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignupGranter/>}/>
+        <Route path="/setup" element={<SetupPage/>}/>
         <Route path="/" element={<SignupWisher/>}/>
         <Route path="/signin" element={<SigninWisher/>}/>
       </Routes>
---
+++ frontend/src/pages/granters/setup/requirement/requirement.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './requirement.css';
+
+function Requirement () {
+
+    return (
+        <div className='requirement'>
+
+            <div className='requirementTitle'>
+                <span className='requirementTitleText'>Sign Up to Become a </span>
+                <span className='requirementTitleGranter'>Granter</span>
+            </div>
+
+            <div className='requirementSubTitle'>
+                <span className='requirementSubTitleText'>Requirements</span>
+            </div>
+
+            <div className='requirementContent'>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Requirement;
