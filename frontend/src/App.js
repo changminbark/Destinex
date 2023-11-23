24440e612e0f124e3aa86a845abcc704497ec213
nl020 <nl020@bucknell.edu>
2023-11-23 16:40:36 -0500

    Complete wish summary page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import SigninWisher from './pages/wishers/signin/Signin';
 import WishProduct from './pages/wishers/wish-product/WishProduct';
 import WishRecipient from "./pages/wishers/wish-recipient/WishRecipient";
 import WishAdditional from "./pages/wishers/wish-additional/WishAdditional";
+import WishSummary from "./pages/wishers/wish-summary/WishSummary";
 
 // Import files for granters
 import SignupGranter from './pages/granters/signup/Signup';
@@ -35,9 +36,10 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/signup" element={<SignupWisher/>}/>
             <Route path="/signin" element={<SigninWisher/>}/>
-            <Route path="/wishproduct" element={<WishProduct/>}/>
-            <Route path="/wishrecipient" element={<WishRecipient/>}/>
-            <Route path="/wishadditional" element={<WishAdditional/>}/>
+            <Route path="/wish-product" element={<WishProduct/>}/>
+            <Route path="/wish-recipient" element={<WishRecipient/>}/>
+            <Route path="/wish-additional" element={<WishAdditional/>}/>
+            <Route path="/wish-summary" element={<WishSummary/>}/>
             <Route path="/granter/signup" element={<SignupGranter/>}/>
             <Route path="/granter/setup" element={<SetupPage/>}/>
             <Route path="/granter/bank" element={<BankAcc/>}/>
