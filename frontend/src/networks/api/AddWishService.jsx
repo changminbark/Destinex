abb206bd4228c9ef8661debf6fa2a266d7f590c2
Chang Min Bark <cb073@bucknell.edu>
2023-11-24 17:12:00 -0500

    worked on middleware

--- frontend/src/networks/api/AddWishService.jsx
+++ frontend/src/networks/api/AddWishService.jsx
@@ -1,7 +1,20 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/jobs';
+const token = localStorage.getItem('jwtToken');
 
-export const addJob = (jobData) => {
-    return axios.post(`${API_URL}`, jobData);
+export const addJob = async (jobData) => {
+    try {
+        return await axios.post(`${API_URL}`, jobData, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        console.log("test")
+        console.error('Axios Error:', error);
+        throw error; // Rethrow the error to propagate it further
+    }
 };
+
+// ... rest of your code
--- frontend/src/networks/hooks/UseAddWish.jsx
+++ frontend/src/networks/hooks/UseAddWish.jsx
@@ -1,28 +1,19 @@
 import * as addWishService from "../api/AddWishService"
-import {useState} from "react";
-import axios from "axios";
 
 export const addJob = async(category, receiverName, receiverPhone, receiverEmail, receiverAddress, description) => {
     const jobData = {
-        "userId": "userPostJob01",
-        "receiverAddressPoint": {
-            "type": "Point",
-            "coordinates": [
-                -75.340,
-                39.984
-            ]
-        },
-        "description": "Job posting test 1"
+        "category": category,
+        "receiverName": receiverName,
+        "receiverPhone": receiverPhone,
+        "receiverEmail": receiverEmail,
+        "receiverAddress": receiverAddress,
+        "description": description
     };
 
-    const token = localStorage.getItem('jwtToken');
 
     try {
-        const response = await axios.post("http://localhost:8080/api/jobs", jobData, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
+        const response = await addWishService.addJob(jobData);
+        // Never reaches this line
         console.log(response.data);
         return true;
     } catch (error) {
