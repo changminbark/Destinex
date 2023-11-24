49b81ca0aa72e7e43015f1f1889c5cacd3330d85
Chang Min Bark <cb073@bucknell.edu>
2023-11-24 18:32:46 -0500

    feat: wish-granted page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import {
 
 import './App.css';
 
+import {AuthProvider} from "./context/AuthContext";
+
 // Import files for wishers
 import SignupWisher from './pages/wishers/signup/Signup';
 import SigninWisher from './pages/wishers/signin/Signin';
@@ -16,18 +18,23 @@ import WishProduct from './pages/wishers/wish-product/WishProduct';
 import WishRecipient from "./pages/wishers/wish-recipient/WishRecipient";
 import WishAdditional from "./pages/wishers/wish-additional/WishAdditional";
 import WishSummary from "./pages/wishers/wish-summary/WishSummary";
+import Home from "./pages/wishers/home/Home";
 
 // Import files for granters
 import SignupGranter from './pages/granters/signup/Signup';
 import SetupPage from './pages/granters/setup/Setup';
 import BankAcc from './pages/granters/bank/Bank';
+import JobList from "./pages/granters/jobs/JobList";
+import WishGranted from "./pages/granters/wish-granted/WishGranted";
+import CongratsPage from "./pages/granters/congrats/CongratsPage";
 
 // Import error page
 import ErrorPage from "./pages/common/error/ErrorPage";
-import CongratsPage from "./pages/granters/congrats/CongratsPage";
-import Home from "./pages/wishers/home/Home";
-import {AuthProvider} from "./context/AuthContext";
-import JobList from "./pages/granters/jobs/JobList";
+
+
+
+
+
 
 function App() {
 
@@ -45,8 +52,9 @@ function App() {
             <Route path="/granter/signup" element={<SignupGranter/>}/>
             <Route path="/granter/setup" element={<SetupPage/>}/>
             <Route path="/granter/bank" element={<BankAcc/>}/>
-            <Route path={"granter/jobs"} element={<JobList/>} />
-            <Route path="/granter/congrats" element={<CongratsPage />}/>
+            <Route path="/granter/jobs" element={<JobList/>} />
+            <Route path="/granter/congrats" element={<CongratsPage/>}/>
+            <Route path="/granter/wish-granted" element={<WishGranted/>}/>
             <Route path="/error" element={<ErrorPage/>}/>
           </Routes>
         </AuthProvider>
