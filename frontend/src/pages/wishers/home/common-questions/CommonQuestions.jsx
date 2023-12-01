9ec001f0caca67ce911ce7d8924b8ce38b57ec36
nl020 <nl020@bucknell.edu>
2023-12-01 01:07:45 -0500

    Complete adding common questions and modifying footer

--- frontend/src/pages/wishers/home/common-questions/CommonQuestions.jsx
+++ frontend/src/pages/wishers/home/common-questions/CommonQuestions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './common_questions.css';
 
-const CommonQuestions = ({ question, answer }) => {
+const CommonQuestions = ({ question, answer, options, additionalNote }) => {
     const [isActive, setIsActive] = useState(false);
 
     return (
@@ -11,11 +11,22 @@ const CommonQuestions = ({ question, answer }) => {
                 <div>{question}</div>
                 <div className="commonQuestionsBtn">{isActive ? '-' : '+'}</div>
             </div>
-            {isActive && <div className="commonQuestionsAnswer">{answer}</div>}
+            {isActive && (
+                <div className="commonQuestionsAnswer">
+                    {answer}
+                    {options && (
+                        <ul>
+                            {options.map((option, index) => (
+                                <li key={index}>{option}</li>
+                            ))}
+                        </ul>
+                    )}
+                    {additionalNote}
+                </div>
+            )}
             <hr></hr>
-
         </div>
-    )
+    );
 }
 
 export default CommonQuestions;
